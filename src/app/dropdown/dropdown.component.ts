db4f5fc432b223df858650e952da8f178fb9ecbb
AndrewThomae <drewthomae@gmail.com>
2022-06-03 11:19:02 -0400

    Full Toolbar Implementation

---
+++ src/app/dropdown/dropdown.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Link } from '../model/link';
+import { OUTS } from '../../data';
+
+@Component({
+  selector: 'app-dropdown',
+  templateUrl: './dropdown.component.html',
+  styleUrls: ['./dropdown.component.css']
+})
+export class DropdownComponent implements OnInit {
+
+  outs = OUTS;
+
+  constructor() {
+   }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/data.ts
@@ -0,0 +1,10 @@
+export const OUTS: any = [
+    {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/andrew-thomae-069a69149/"
+    },
+    {
+    name: "GitHub",
+    link: "https://github.com/AndrewThomae"
+  }
+];
